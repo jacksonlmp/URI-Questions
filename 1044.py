6f9b9151d4bca392ac1f8f8ce213961dbc30b1de
Jackson <45649262+jacksonlmp@users.noreply.github.com>
2019-09-28 01:50:31 -0300

    Delete 1044.py

--- 1044.py
+++
@@ -1,7 +0,0 @@
-entrada = raw_input("")
-entrada = entrada.split(" ")
-
-if(int(entrada[0]) % int(entrada[1]) == 0 or int(entrada[1]) % int(entrada[0]) == 0):
-    print "Sao Multiplos"
-else:
-    print "Nao sao Multiplos"
